b2b9e943f9fab997394fd2d04a1a829c85e3ead2
Eeo Jun <141bytes@gmail.com>
2017-11-22 14:20:46 +0000

    try to get it working on gh-pages

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,13 @@ cd docs/ || exit
 g3-build
 cd ..
 
+misc/prefix-gh-pages docs/_g3/dst/ /g3
+
 mkdir .tmp
 cp -r docs/_g3/dst/ .tmp
 
-git checkout gh-pages
-rm -rf *
+git checkout gh-pages || exit
+rm -rf ./*
 mv .tmp/* .
 git commit -am "build $commit_hash"
 git push
